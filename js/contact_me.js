1e3881afd5db16e88615585402fd0b47f13b4088
Kiuby88 <kiuby88.programming@gmail.com>
2018-08-16 23:14:44 +0200

    Add

--- js/contact_me.js
+++ js/contact_me.js
@@ -83,7 +83,7 @@ cod1='lasT';
         $('#success').html("<div class='alert alert-danger'>");
           $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
-          $('#success > .alert-danger').append($("<strong>").text("Sorry some codes are not correct. Try again!!"));
+          $('#success > .alert-danger').append($("<strong>").text("Sorry some codes are not correct. Try again!! --Check the ORDER--"));
           $('#success > .alert-danger').append('</div>');
           //clear all fields
           $('#contactForm').trigger("reset");
